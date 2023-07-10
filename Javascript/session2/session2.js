d9bf37792c93ac915a09cf33de21a677a803e9e7
RgnDunes <divyansh.singh@razorpay.com>
2023-07-10 22:17:36 +0530

    Add session 2 code

---
+++ Javascript/session2/session2.js
@@ -0,0 +1,66 @@
+const person = [
+  "Divyansh",
+  25,
+  "Razorpay",
+  "SDE",
+  {
+    street: "Jakasandra",
+    houseNo: 53,
+    pin: 560034,
+  },
+];
+
+// const hola = person[0];
+// const age = person[1];
+
+// const [chola, age1, zainul] = person;
+
+// console.log("hola : ", hola);
+// console.log("chola : ", chola);
+// console.log("age : ", age);
+// console.log("age1 : ", age1);
+// console.log("zainul : ", zainul);
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+
+// const arr3 = [...arr1, ...arr2];
+// console.log(arr3);
+
+// const spreadedPerson = [...person];
+
+// spreadedPerson[0] = "Divyansh Singh";
+// spreadedPerson[4].houseNo = 55;
+
+// console.log("person : ", person);
+// console.log("spreadedPerson : ", spreadedPerson);
+
+// const numbers = [5, 4, 3, 2, 1];
+
+// numbers.forEach((number) => {
+//   console.log("forEach : ", number);
+// });
+
+// const mappedData = numbers.map((number) => {
+//   if (number % 2 === 0) return number;
+// });
+// console.log("mappedData : ", mappedData);
+
+// const evenNumbers = numbers.filter((number) => {
+//   if (number % 2 === 0) return number;
+// });
+
+// console.log("evenNumbers : ", evenNumbers);
+
+// numbers.sort();
+// console.log(numbers);
+
+// const foundNumber = numbers.find((number) => {
+//   return number > 3;
+// });
+
+// console.log("foundNumber : ", foundNumber);
+
+// const a = { b: 1, c: 2 };
+// a.d = a;
+// console.log(a);
